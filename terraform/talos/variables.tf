64b796f55d1d7c19ce74b14032239d09f64f9458
Philipp Matthias Hauptmann <haupha08@gmail.com>
2026-08-04 17:54:50 +0200

    chore(variables): update talos version to v1.13.8

--- terraform/talos/variables.tf
+++ terraform/talos/variables.tf
@@ -22,7 +22,7 @@ variable "gateway" {
 variable "talos_version" {
   description = "Talos version used for the factory installer image."
   type        = string
-  default     = "v1.13.7"
+  default     = "v1.13.8"
 }
 
 variable "kubernetes_version" {
