9593303c9a0052e752cce8d0e5d795a2adb3f005
Philipp Matthias Hauptmann <haupha08@gmail.com>
2026-08-18 19:08:33 +0200

    fix: update default Talos and Kubernetes versions

--- terraform/talos/variables.tf
+++ terraform/talos/variables.tf
@@ -22,13 +22,13 @@ variable "gateway" {
 variable "talos_version" {
   description = "Talos version used for the factory installer image."
   type        = string
-  default     = "v1.13.8"
+  default     = "v1.13.9"
 }
 
 variable "kubernetes_version" {
   description = "Target Kubernetes version."
   type        = string
-  default     = "v1.36.1"
+  default     = "v1.36.3"
 }
 
 variable "schematic_file" {
