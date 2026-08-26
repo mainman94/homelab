5c3d181f711dc2a6b2a2b220340962965a1724d3
Philipp Hauptmann <philipp.hauptmann@fullstacks.io>
2026-08-26 12:38:23 +0200

    feat: add launch_url for multiple OIDC clients in Pocket ID configuration

--- terraform/pocket-id/main.tf
+++ terraform/pocket-id/main.tf
@@ -24,12 +24,14 @@ locals {
     argo-cd = {
       client_id     = "9ef757ce-42bd-4c89-be46-3f0bbeff4509"
       callback_urls = ["https://argo.hauptmann.dev/api/dex/callback", "https://localhost:8085/auth/callback"]
+      launch_url    = "https://argo.hauptmann.dev"
       groups        = ["homelab"]
     }
     opencloud = {
       client_id            = "cc227522-08e4-45c4-8b33-47637d674452"
       callback_urls        = ["https://cloud.hauptmann.dev/oidc-callback.html", "https://cloud.hauptmann.dev", "https://cloud.hauptmann.dev/oidc-silent-redirect.html"]
       logout_callback_urls = ["https://cloud.hauptmann.dev"]
+      launch_url           = "https://cloud.hauptmann.dev"
       is_public            = true
       pkce_enabled         = true
       groups               = ["homelab", "opencloud_spaceadmins"]
@@ -38,6 +40,7 @@ locals {
       client_id     = "OpenCloudDesktop"
       name          = "OpenCloud Desktop"
       callback_urls = ["http://127.0.0.1", "http://localhost"]
+      launch_url    = "https://cloud.hauptmann.dev"
       is_public     = true
       pkce_enabled  = true
     }
@@ -45,6 +48,7 @@ locals {
       client_id     = "OpenCloudAndroid"
       name          = "OpenCloud Android"
       callback_urls = ["oc://android.opencloud.eu"]
+      launch_url    = "https://cloud.hauptmann.dev"
       is_public     = true
       pkce_enabled  = true
     }
@@ -52,6 +56,7 @@ locals {
       client_id     = "OpenCloudIOS"
       name          = "OpenCloud iOS"
       callback_urls = ["oc://ios.opencloud.eu"]
+      launch_url    = "https://cloud.hauptmann.dev"
       is_public     = true
       pkce_enabled  = true
     }
@@ -59,6 +64,7 @@ locals {
       client_id     = "4123dfc3-cac2-4639-ba61-188046e20605"
       name          = "git.hauptmann.dev"
       callback_urls = ["https://git.hauptmann.dev/user/oauth2/openid/Gitea", "https://git.hauptmann.dev/user/oauth2/pocketid/callback"]
+      launch_url    = "https://git.hauptmann.dev"
       groups        = ["homelab"]
     }
     kargo = {
@@ -83,18 +89,21 @@ locals {
       client_id     = "56540b0f-e214-49c4-b25a-954c6af5e8c8"
       name          = "Cloudflare"
       callback_urls = ["https://eggenberg-hauptmann.cloudflareaccess.com/cdn-cgi/access/callback"]
+      launch_url    = "https://eggenberg-hauptmann.cloudflareaccess.com"
       groups        = ["homelab"]
     }
     audiobookshelf = {
       client_id     = "405d667b-f766-4b7f-9cde-f075899f31bb"
       name          = "Audiobookshelf"
       callback_urls = ["https://audiobookshelf.hauptmann.dev/audiobookshelf/auth/openid/mobile-redirect", "https://audiobookshelf.hauptmann.dev/audiobookshelf/auth/openid/callback"]
+      launch_url    = "https://audiobookshelf.hauptmann.dev"
       groups        = ["homelab", "admin"]
     }
     grafana = {
       client_id     = "7a8642c3-f7a1-4790-ac28-8f14e00a2779"
       name          = "Grafana"
       callback_urls = ["https://grafana.hauptmann.dev/login/generic_oauth"]
+      launch_url    = "https://grafana.hauptmann.dev"
       groups        = ["homelab"]
     }
   }
@@ -107,7 +116,7 @@ resource "pocketid_client" "this" {
   name                 = try(each.value.name, each.key)
   callback_urls        = each.value.callback_urls
   logout_callback_urls = try(each.value.logout_callback_urls, [])
-  launch_url           = try(each.value.launch_url, null)
+  launch_url           = each.value.launch_url
   is_public            = try(each.value.is_public, false)
   pkce_enabled         = try(each.value.pkce_enabled, false)
   allowed_user_groups  = [for g in try(each.value.groups, []) : pocketid_group.this[g].id]
