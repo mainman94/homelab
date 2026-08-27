1fe06375ddc817755deb4ff392cc6c9bbf552f71
Philipp Hauptmann <philipp.hauptmann@fullstacks.io>
2026-08-27 13:09:58 +0200

    feat: set Dockhand OIDC client to private access

--- terraform/pocket-id/main.tf
+++ terraform/pocket-id/main.tf
@@ -110,7 +110,7 @@ locals {
       name          = "Dockhand"
       callback_urls = ["https://dockhand.hauptmann.dev/api/auth/oidc/callback"]
       launch_url    = "https://dockhand.hauptmann.dev"
-      is_public     = true
+      is_public     = false
       pkce_enabled  = true
       groups        = ["homelab"]
     }
