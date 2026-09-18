df84112adfea1edc35338989ffe3a755d124516f
Philipp Matthias Hauptmann <haupha08@gmail.com>
2026-09-18 20:20:03 +0200

    fix(talos): remove static hostname config patches to avoid conflicts

--- terraform/talos/main.tf
+++ terraform/talos/main.tf
@@ -15,19 +15,6 @@ locals {
     )
   }
 
-  # Static machine.network.hostname conflicts with the default HostnameConfig
-  # auto-generation doc unless it's explicitly disabled.
-  controlplane_hostname_config_patches = {
-    for name, node in var.controlplane_nodes :
-    name => trimspace(<<-EOT
-      apiVersion: v1alpha1
-      kind: HostnameConfig
-      auto: off
-    EOT
-    )
-    if try(node.node_name, null) != null
-  }
-
   controlplane_patches = {
     for name, node in var.controlplane_nodes :
     name => yamlencode({
